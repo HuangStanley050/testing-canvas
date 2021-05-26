0774a87f3c64510b5b594dc155550ae2d6ee9285
Stanley Huang <infamous_godhand@yahoo.com>
2021-05-26 16:45:20 +1000

    refactor arcMove function

--- src/index.js
+++ src/index.js
@@ -1,27 +1,33 @@
 let can = document.getElementById("canvas"),
   spanPercent = document.getElementById("percent"),
-  c = can.getContext("2d");
+  spanDegree = document.getElementById("degree");
+c = can.getContext("2d");
 
 let posX = can.width / 2,
   posY = can.height / 2,
   fps = 1000 / 200,
   percent = 0,
   onePercent = 360 / 100,
-  progress = 90,
-  result = onePercent * progress;
+  percentageToStop = onePercent * 65;
 
 c.lineCap = "round";
 
-function arcMove() {
+const radianConversion = Math.PI / 180;
+let startAngle = radianConversion * 90;
+let endAngle = radianConversion * (90 + 360);
+
+function arcMove(percentageToStop, startAngle, endAngle) {
   let degree = 0;
-  let startAngle = (Math.PI / 180) * 90;
-  let endAngle = (Math.PI / 180) * (90 + 360);
-  let acrInterval = setInterval(() => {
+
+  let arcInterval = setInterval(() => {
+    let end;
     degree += 1;
+    end = radianConversion * (90 + degree);
     c.clearRect(0, 0, can.width, can.height);
     percent = parseInt(degree / onePercent, 10);
 
     spanPercent.innerHTML = percent.toFixed();
+    spanDegree.innerHTML = degree.toFixed();
 
     // this is the base circle, aka the gray area, the unfulfilled
     c.beginPath();
@@ -33,17 +39,17 @@ function arcMove() {
     // this is the part where it fills up
 
     c.beginPath();
+    c.arc(posX, posY, 90, startAngle, end);
     c.strokeStyle = "red";
     c.lineWidth = "10";
-    c.arc(posX, posY, 90, startAngle, (Math.PI / 180) * (90 + degree));
     c.stroke();
 
     //console.log(`this is degree ${degree}`);
     console.log(
       `this is percent ${parseInt(percent.toFixed())} degree -- ${degree}`
     );
-    if (degree >= result) clearInterval(acrInterval);
+    if (degree >= percentageToStop) clearInterval(arcInterval);
   }, fps);
 }
 
-arcMove();
+arcMove(percentageToStop, startAngle, endAngle);
